0d4b8db647a8208fd0224f50f104ea8c92d0f989
Yaniv Raz <yanivmiz77@gmail.com>
2026-09-07 16:02:29 +0300

    Combine Zaviyot branding and navigation into compact reader header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import { JerusalemPanoramaSection } from "@/components/JerusalemPanoramaSection"
 import { WorksheetsBookletBook } from "@/components/WorksheetsBookletBook";
 import { FinaleSection } from "@/components/FinaleSection";
 import { SiteFooter } from "@/components/SiteFooter";
+import styles from "./HomeChrome.module.css";
 
 export default function HomePage() {
   return (
     <>
-      <div className="home-chrome">
+      <div className={`${styles.chrome} home-chrome`}>
         <TopBar />
         <SiteNav />
       </div>
